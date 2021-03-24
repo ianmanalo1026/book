d002d95770e3e6c08bb557e0600af31e72868f71
Ian <ian.manalo1026@gmail.com>
2021-03-25 00:32:57 +0800

    new project

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    
+    
+    def __str__(self):
+        return self.title
